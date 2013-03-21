2f4f3501059c394b09c2ca16924d46988735a76b
Vitaliy Beskrovny <bva@aejis.eu>
2013-03-21 06:40:09 +0200

    + HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,16 @@
+# encoding: UTF-8
 class HomeController < ApplicationController
+  before_filter :authenticate_user!
 
   def index
 
   end
 
+  def input
+    raise(CanCan::AccessDenied, 'В доступе отказано: пользователь не привязан ни к одному Дому.') if current_user.team.empty?
+
+    flash[:message] = CodeFacade.input(params[:code_string])
+
+    render :index
+  end
 end
