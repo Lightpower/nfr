5269206ee67ddace95a03168bcbdede56388cd8f
Vitaliy Beskrovny <bva@aejis.eu>
2013-04-01 06:07:27 +0300

    + zone_holder tests and factory

---
+++ spec/factories/zone_holders.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :zone_holder do
+    zone_id nil
+    team_id nil
+    team_code_id nil
+    time "2013-04-01 02:46:25"
+  end
+end
