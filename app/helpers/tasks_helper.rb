8799a566b7c536c1196c01d0e9e26e66b56e11fe
Vitaliy Beskrovny <bva@aejis.eu>
2013-04-01 06:06:18 +0300

    * renaming the file

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,11 +1,11 @@
 # encoding: UTF-8
-module TaskHelper
+module TasksHelper
 
   ##
   # Show preview for not allowed tasks
   #
   def show_preview(task)
-    result = preview
+    result = task.preview
     result << "<br>Код доступа к заданию: #{task.access_code.code_strings.map(&:data).join(", ")}"
     result << "<br>Стоимость задания: #{- task.access_code.try(:bonus)} кодов. В задании есть:"
     result << "<br>- реальных кодов: #{task.codes.where("ko != 'null'").size}"
