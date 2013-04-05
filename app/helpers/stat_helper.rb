2ca4e6f3d552ba9234243c34226c50a7869fb118
Vitaliy Beskrovny <bva@aejis.eu>
2013-04-05 17:36:01 +0300

    - show_stat_row method

--- app/helpers/stat_helper.rb
+++ app/helpers/stat_helper.rb
@@ -2,41 +2,6 @@
 module StatHelper
 
   ##
-  # Make table row with zone information
-  #
-  # Params:
-  # - params {Hash} Data about zone
-  #
-  # Example of data:
-  #     {
-  #       name: 'Casterly Rocks',
-  #       holder: {
-  #         name: 'Lannisters',
-  #         codes: 11,
-  #         time: '03:03:17',
-  #         image: '/images/starks.png',
-  #         team_codes: 12,
-  #       },
-  #     }
-  #
-  def show_stat_row(params)
-    result = ""
-    result << content_tag(:td, content_tag(:b, params[:name]), class: 'zone_name')
-    if params[:holder][:image].present?
-      result << content_tag(:td, content_tag(:img, src: params[:holder][:image], width: 16, height: 16))
-    else
-      result << content_tag(:td)
-    end
-    result << content_tag(:td, params[:holder][:name])
-    result << content_tag(:td, params[:holder][:codes])
-    result << content_tag(:td, params[:holder][:time])
-    result << content_tag(:td, params[:team_codes])
-
-    content_tag(:tr, result.html_safe)
-  end
-
-
-  ##
   # Make div block with zone information
   #
   # Params:
