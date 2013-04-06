5815535f715cb7ed7b6c07ad756a81efd707d9e6
Vitaliy Beskrovny <bva@aejis.eu>
2013-04-06 17:53:36 +0300

    * remember expanding of openable divs

--- app/assets/javascripts/interface.js
+++ app/assets/javascripts/interface.js
@@ -1,16 +1,26 @@
 $(function() {
   $("a.openable").bind("click", function(e) {
-    var submenu = $(this).parents('.openable_holder').first().children("div.openable"),
+    var id = this.id,
+      submenu = $(this).parents('.openable_holder').first().children("div.openable"),
       this_text = $(this).text();
     if(submenu.hasClass("hidden")) {
       submenu.removeClass("hidden");
       this_text = "- " + this_text.split("+")[1];
+      NFR.openable.open(id);
     } else {
       submenu.addClass("hidden");
       this_text = "+" + this_text.split("- ")[1];
+      NFR.openable.close(id);
     }
     $(this).text(this_text);
 
     e.preventDefault();
   });
+
+  var openable_list = NFR.openable.array(),
+      i = 0;
+
+  for(; i<openable_list.length; ++i) {
+    $("a#" + openable_list[i]).click();
+  }
 });
