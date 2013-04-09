c1f043a3d52fa7bf6af0e37b3be3387b952be302
Vitaliy Beskrovny <bva@aejis.eu>
2013-04-09 19:18:44 +0300

    + inicorn_startup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,37 +1,39 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-# require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (http://rvm.io)
+# require 'mina/rbenv' # for rbenv support. (http://rbenv.org)
+# require 'mina/rvm' # for rvm support. (http://rvm.io)
 
 # Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
+# domain - The hostname to SSH to.
+# deploy_to - Path to deploy into.
+# repository - Git repo to clone from. (needed by mina/git)
+# branch - Branch name to deploy. (needed by mina/git)
 
 set :domain, 'smile-team.info'
 set :deploy_to, '/srv/http/got.smile-team.info'
 set :repository, 'git@github.com:Lightpower/nfr.git'
 set :branch, 'master'
 
+set_default :unicorn_startup, "bundle exec unicorn"
+
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_paths, ['config/database.yml', 'log']
 
-# Optional settings:
-   set :user, 'deploy'    # Username in the server to SSH to.
-#   set :port, '30000'     # SSH port number.
+                    # Optional settings:
+set :user, 'deploy' # Username in the server to SSH to.
+                    # set :port, '30000' # SSH port number.
 
-# This task is the environment that is loaded for most commands, such as
-# `mina deploy` or `mina rake`.
+                    # This task is the environment that is loaded for most commands, such as
+                    # `mina deploy` or `mina rake`.
 task :environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .rbenv-version to your repository.
-  # invoke :'rbenv:load'
+# If you're using rbenv, use this to load the rbenv environment.
+# Be sure to commit your .rbenv-version to your repository.
+# invoke :'rbenv:load'
 
-  # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
+# For those using RVM, use this to load an RVM version@gemset.
+# invoke :'rvm:use[ruby-1.9.3-p125@default]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
@@ -45,30 +47,38 @@ task :setup => :environment do
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
 
   queue! %[touch "#{deploy_to}/shared/config/database.yml"]
-  #queue!  %[-----> Be sure to edit 'shared/config/database.yml'.]
+#queue! %[-----> Be sure to edit 'shared/config/database.yml'.]
 end
 
 desc "Deploys the current version to the server."
 task :deploy => :environment do
   deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
+# Put things that will set up an empty directory into a fully set-up
+# instance of your project.
+    invoke :'unicorn:stop'
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
 
-    to :launch do
-      queue 'touch tmp/restart.txt'
+    invoke :'unicorn:start'
+  end
+end
+
+namespace :unicorn do
+  %w(start stop).each do |command|
+    desc "#{command.capitalize} Unicorn"
+    task command do
+      queue %{echo "-----> #{command.capitalize}ing Unicorn..."}
+      queue %{#{unicorn_startup} #{command}}
     end
   end
 end
 
 # For help in making your deploy script, see the Mina documentation:
 #
-#  - http://nadarei.co/mina
-#  - http://nadarei.co/mina/tasks
-#  - http://nadarei.co/mina/settings
-#  - http://nadarei.co/mina/helpers
-
+# - http://nadarei.co/mina
+# - http://nadarei.co/mina/tasks
+# - http://nadarei.co/mina/settings
+# - http://nadarei.co/mina/helpers
