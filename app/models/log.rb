623cb9bfaa3f905981f59e6a869554a7d04d721a
Vitaliy Beskrovny <bva@aejis.eu>
2013-04-11 15:17:23 +0300

    + method

--- app/models/log.rb
+++ app/models/log.rb
@@ -3,6 +3,10 @@ class Log < ActiveRecord::Base
   belongs_to :team
   belongs_to :code
 
-
   attr_accessible :data, :team, :team_id, :login, :result_code, :code, :code_id
+
+  def can_show_info?
+    ! [Code::STATES.index(:not_found), Code::STATES.index(:not_available)].include?(self.result_code)
+  end
+
 end
