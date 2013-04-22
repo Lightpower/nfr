f484ceeb7ac6b1e9e9f58f04a892235d5e1f788c
Vitaliy Beskrovny <bva@aejis.eu>
2013-04-22 16:04:58 +0300

    - reprocess; + code passing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,14 +5,14 @@ class HomeController < ApplicationController
 
   def index
 
-    ## Codes processing
-    #if params[:code_string].present?
-    #  @results = CodeFacade.input({code_string: params[:code_string][:code], user: current_user})
-    #elsif params[:task_id].present?
-    #  @results = [CodeFacade.get_hint({task_id: params[:task_id], user: current_user})]
-    #else
-    #  @results = nil
-    #end
+    # Codes processing
+    if params[:code_string].present?
+      @results = CodeFacade.input({code_string: params[:code_string][:code], user: current_user})
+    elsif params[:task_id].present?
+      @results = [CodeFacade.get_hint({task_id: params[:task_id], user: current_user})]
+    else
+      @results = nil
+    end
 
     @zones = current_user.team.zones
 
@@ -22,15 +22,6 @@ class HomeController < ApplicationController
   def stat
     render 'stat/index', locals: {data: Stat.total(current_user.team)}#, layout: 'layouts/statistics'
   end
-
-  def reprocess
-    Stat.reprocess(current_user)
-
-    @zones = current_user.team.zones
-
-    render 'zones/index'
-  end
-
 
   private
 
