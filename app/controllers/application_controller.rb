2471e65c7b60913bfdb5c2bc1db77b88e8140492
Vitaliy Beskrovny <bva@aejis.eu>
2013-04-29 10:11:58 +0300

    + current_game helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
+
+  def current_game
+    Game.find_by_id(params[:game_id]).first
+  end
 end
