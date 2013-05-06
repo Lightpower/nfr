7f6b177a4811cc6e826c52012b6304eb3a74a504
Vitaliy Beskrovny <bva@aejis.eu>
2013-05-06 18:22:54 +0300

    - don't user current_game helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,4 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, :alert => exception.message
   end
 
-  def current_game
-    Game.find_by_id(params[:game_id]).first
-  end
 end
