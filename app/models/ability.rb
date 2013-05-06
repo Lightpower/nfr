c4edfe796997910f859f84e227453e614280218b
Vitaliy Beskrovny <bva@aejis.eu>
2013-05-06 18:26:25 +0300

    + all abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,43 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new
+
+    if user.role == 'admin'
+      can :manage, :all
+    else
+
+      # Guest or User without team
+      if user.team.blank?
+        cannot :read, Game
+      end
+
+      # Registered user
+      if user.id
+        can :manage, User,     id: user.id
+        can :create, Team
+        can :read,   Team
+      end
+
+      # Captain
+      if user.team.try(:captain)
+        can :manage, Team,        captain: user
+        can :manage, User,        team_id: user.team_id
+        can :manage, TeamRequest, team_id: user.team_id
+        can :manage, GameRequest, team_id: user.team_id
+      end
+
+      # Access to Game
+      if user.team.present?
+        team = user.team
+        if team.present?
+          can [:show, :stat], Game do |game|
+            game.teams.include?(team)
+          end
+        end
+      end
+    end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
