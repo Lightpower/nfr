68c4c6726c84a9e628291395625d8688f0ade364
Vitaliy Beskrovny <bva@aejis.eu>
2013-05-06 22:12:07 +0300

    + Other formats

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,14 @@ class HomeController < ApplicationController
   def index
     @games = Game.actual
     if params[:games]
-      @games = @games.where("lower(format) = ?", params[:games][:format].downcase) if params[:games][:format]
+      # game format
+      case params[:games][:format]
+        when "other"
+          @games = @games.where("lower(format) NOT IN (?)", Game::CSS_CLASSES)
+        when nil
+        else
+          @games = @games.where("lower(format) = ?", params[:games][:format].downcase)
+      end
     end
   end
 
