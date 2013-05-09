19b9f281e9a7fc45eaa9123b072d58e391f2ab17
Vitaliy Beskrovny <bva@aejis.eu>
2013-05-09 05:02:55 +0300

    + game_config migration

---
+++ db/migrate/20130509001828_create_game_config.rb
@@ -0,0 +1,16 @@
+class CreateGameConfig < ActiveRecord::Migration
+  def change
+    create_table :game_configs do |t|
+      t.integer :time
+      t.integer :bonus
+
+      t.references :game
+    end
+
+    add_index :game_configs, :game_id, null: false, unique: true
+
+    if Rails.env != 'test'
+      Game.all.each { |game| GameConfig.create(game_id: game.id, time: 1800, bonus: 45)}
+    end
+  end
+end
