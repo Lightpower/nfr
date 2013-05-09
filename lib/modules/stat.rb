988f03a5a44105f987b1bc73e5dc4bdc40c25d69
Vitaliy Beskrovny <bva@aejis.eu>
2013-05-09 05:07:24 +0300

    * update and bug fixing

--- lib/modules/stat.rb
+++ lib/modules/stat.rb
@@ -4,10 +4,10 @@ module Stat
   class << self
     ##
     # Show the intermediate statistics based on Game's game_type
-    # Please see private methods "subtotal_*" for more detailed description
+    # Please see methods "subtotal_*" for more detailed description
     #
     # Params:
-    # - params {Hash} - Hash with Game and
+    # - params {Hash} - Hash with Game and other possible parameters
     #
     def subtotal(params)
       game = params[:game]
@@ -18,6 +18,25 @@ module Stat
         send(subtotal_method, params)
       else
         { error: "Game with game_type=\"#{game.game_type}\" cannot show subtotal!"}
+      end
+    end
+
+    ##
+    # Show the final statistics based on Game's game_type
+    # Please see methods "total_*" for more detailed description
+    #
+    # Params:
+    # - params {Hash} - Hash with Game and other possible parameters
+    #
+    def total(params)
+      game = params[:game]
+      return {error: 'Game not found'} unless game
+
+      total_method = "total_#{game.game_type}"
+      if respond_to? total_method
+        send(total_method, params)
+      else
+        { error: "Game with type=\"#{game.game_type}\" cannot show total!"}
       end
     end
 
@@ -109,15 +128,15 @@ module Stat
     ##
     # Bonuses of holding for each team
     #
-    def hold_bonuses
+    def hold_bonuses(game)
       result = {}
-      Team.all.each do |team|
+      game.teams.each do |team|
         result[team.id] = {}
-        Zone.all.each do |zone|
+        game.zones.each do |zone|
           result[team.id][zone.id] = {}
           bonus = 0
           last_hold = nil
-          ZoneHolder.where(zone_id: zone.id).order(:created_at).each do |hold|
+          ZoneHolder.where(game_id: game.id, zone_id: zone.id).order(:created_at).each do |hold|
             if hold.team_id != last_hold.try(:team_id)
               if last_hold.try(:team_id) == team.id
                 bonus += ((hold.created_at - last_hold.created_at).to_i / 30) * 45
@@ -126,7 +145,7 @@ module Stat
             end
           end
           if last_hold.try(:team_id) == team.id
-            bonus += ((Time.parse('2013-04-14 08:00:00') - last_hold.created_at).to_i / 1800) * 45
+            bonus += ((Time.parse('2013-04-14 08:00:00') - last_hold.created_at).to_i / game.hold_time) * game.hold_bonus
           end
           result[team.id][zone.id] = bonus
         end
@@ -727,7 +746,7 @@ module Stat
       result = {}
       teams.each do |t|
         result[t] = {}
-        zones.each { |z| result[t][z] = {sum: 0, time_bonus: 0} }
+        zones.each { |z| result[t][z] = {sum: 0, bonus: 0} }
 
       end
 
@@ -752,7 +771,7 @@ module Stat
               if old_team.present?
                 old_bonus = 0
                 old_bonus = ((time - zone_holders[new_zone][:time]).to_i / 1800) * 45 if zone_holders[new_zone][:time].present?
-                result[old_team][new_zone][:time_bonus] += old_bonus
+                result[old_team][new_zone][:bonus] += old_bonus
               end
 
               # change holders data
@@ -774,7 +793,7 @@ module Stat
           old_team = zone_holders[zone][:team]
           old_bonus = 0
           old_bonus = ((time - zone_holders[zone][:time]).to_i / 1800) * 45 if zone_holders[zone][:time].present?
-          result[old_team][zone][:time_bonus] += old_bonus
+          result[old_team][zone][:bonus] += old_bonus
         rescue Exception => e
           puts [zone, e]
         end
@@ -786,12 +805,96 @@ module Stat
         bonus = 0
         zones.each do |zone|
           sum += result[team][zone][:sum] || 0
-          bonus += result[team][zone][:time_bonus] || 0
+          bonus += result[team][zone][:bonus] || 0
         end
-        result[team]["8"] = {sum: sum, time_bonus: bonus}
+        result[team]["8"] = {sum: sum, bonus: bonus}
       end
 
       {bonuses: result, holders: zone_holders}
     end
+
+    ##
+    # Total statistics of the Game with game_type='zones'
+    #
+    # Params:
+    # - params {Hash} - Hash with Game:
+    #  - game {Game} - Game whose results should be created
+    #
+    def total_zones(params)
+      game = params[:game]
+      log_filter = [Code::STATES.index(:accepted), Code::STATES.index(:accessed), Code::STATES.index(:hint_accessed), Code::STATES.index(:attached)]
+      log = game.logs.where(result_code: log_filter).order(:created_at)
+      zones = game.zones.map(&:id)
+      teams = game.teams.map(&:id)
+      finish_time = game.finish_date
+
+      zone_holders = {}
+      zones.each {|z| zone_holders[z] = {team: nil, sum: 0, time: nil} }
+      result = {}
+      teams.each do |t|
+        result[t] = {}
+        zones.each { |z| result[t][z] = {sum: 0, bonus: 0} }
+      end
+
+      log.each do |l|
+        new_team = l.team_id
+        amount = l.team_code.bonus
+        new_zone = l.team_code.zone.try(:id)
+        time = l.created_at
+        next if (new_team[0] == 'a') || (new_zone.blank?) || (amount == 0) || (time > finish_time)
+
+        begin
+          result[ new_team ][new_zone][:sum] += amount
+          # Does the new team have the best sum in zone?
+          if result[ new_team ][new_zone][:sum] > zone_holders[new_zone][:sum]
+            # Check if this is the new holder
+            if zone_holders[new_zone][:team] == new_team
+              zone_holders[new_zone][:sum] = result[ new_team ][new_zone][:sum]
+            else
+              # count bonus
+              old_team = zone_holders[new_zone][:team]
+              if old_team.present?
+                old_bonus = 0
+                old_bonus = ((time - zone_holders[new_zone][:time]).to_i / game.hold_time) * game.hold_bonus if zone_holders[new_zone][:time].present?
+                result[old_team][new_zone][:bonus] += old_bonus
+              end
+
+              # change holders data
+              zone_holders[new_zone][:time] = time
+              zone_holders[new_zone][:team] = new_team
+              zone_holders[new_zone][:sum] = result[ new_team ][new_zone][:sum]
+            end
+
+          end
+        rescue Exception => e
+          puts [l, e]
+        end
+      end
+
+      zones.each do |zone|
+        begin
+          old_team = zone_holders[zone][:team]
+          old_bonus = 0
+          old_bonus = ((finish_time - zone_holders[zone][:time]).to_i / game.hold_time) * game.hold_bonus if zone_holders[zone][:time].present?
+          result[old_team][zone][:bonus] += old_bonus
+        rescue Exception => e
+          puts [zone, e]
+        end
+      end
+
+      # total
+      teams.each do |team|
+        sum = 0
+        bonus = 0
+        zones.each do |zone|
+          sum += result[team][zone][:sum] || 0
+          bonus += result[team][zone][:bonus] || 0
+        end
+        result[team]["total"] = {sum: sum, bonus: bonus}
+      end
+
+      {bonuses: result, holders: zone_holders}
+    end
+
   end
 end
