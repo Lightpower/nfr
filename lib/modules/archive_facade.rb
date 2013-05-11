3629a472d7026443e4c550b7a5187b73c04f1492
Vitaliy Beskrovny <bva@aejis.eu>
2013-05-12 02:15:34 +0300

    - Log form archive list - it is being archived separately; + caopy game from archive

--- lib/modules/archive_facade.rb
+++ lib/modules/archive_facade.rb
@@ -13,16 +13,38 @@ module ArchiveFacade
         team_table.merge!({team.id => archive_team.id})
       end
       # Copying the tables and clear them
-      %w(Zone Task Code CodeString Hint TeamBonus TeamCode TeamHint TeamZone Log).each do |table|
+      %w(Zone Task Code CodeString Hint TeamBonus TeamCode TeamHint TeamZone).each do |table|
         copy_and_clear_table({ table: table, game: game, team_table: team_table })
       end
       # Copy logs - successful records only
-      copy_and_clear_table({ table: table, game: game, team_table: team_table,
+      copy_and_clear_table({ table: "Log", game: game, team_table: team_table,
         result_filter: [Code::STATES.index(:accepted), Code::STATES.index(:accessed), Code::STATES.index(:hint_accessed), Code::STATES.index(:attached)]
                            })
       game.is_archived = true
       game.is_active = false
       game.save
+      game
+    end
+
+    ##
+    # Create new game by copying one from archive
+    #
+    def copy_from_archive(game)
+      return false unless game.is_archived
+
+      new_game = game.dup
+      new_game.number *= 1000
+      new_game.name += " (копия)"
+      new_game.is_active = false
+      new_game.is_archived = false
+      new_game.save
+
+      # Zones with ALL stuff
+      game.archive_zones.each do |archive_zone|
+        copy_zone(archive_zone, new_game)
+      end
+
+      new_game
     end
 
     private
@@ -39,19 +61,131 @@ module ArchiveFacade
       archive_class = ("Archive#{table_name}").constantize
 
       main_class.where(game_id: game.id).each do |row|
-        archive_instance = archive_class.new
-        main_class.column_names.each do |column|
-          if result_filter.include?(row.result_code)
+
+        condition = result_filter.blank? || result_filter.include?(row.result_code) # check the result_filter
+        condition &&= archive_class.find_by_id(row.id).blank?    # check if this record is already archived
+        if condition
+          archive_instance = archive_class.new
+          main_class.column_names.each do |column|
             if column == 'team_id'
               archive_instance.send( "#{column}=", team_table[row.send(column)] )
             else
               archive_instance.send("#{column}=", row.send(column))
             end
           end
+          archive_instance.save
         end
-        archive_instance.save
       end
       main_class.where(game_id: game.id).map(&:delete)
+    end
+
+    ##
+    # Copy Zone with all stuff
+    #
+    def copy_zone(archive_zone, parent)
+      return nil unless archive_zone
+
+      # Create access code if present
+      access_code = copy_code(archive_zone.access_code, parent)
+      # Create Zone
+      zone = Zone.create(
+          number: archive_zone.number,
+          name: archive_zone.name,
+          image_url: archive_zone.image_url,
+          preview: archive_zone.preview,
+          code_id: access_code.try(:id),
+          game_id: parent.id
+      )
+      # Create tasks
+      archive_zone.archive_tasks.each {|item| copy_task(item, zone) }
+
+      zone
+    end
+
+
+    ##
+    # Copy Task with all codes, hints and included tasks
+    #
+    def copy_task(archive_task, parent)
+      return nil unless archive_task
+      zone_id = parent.is_a?(Zone) ? parent.id : nil
+      task_id = parent.is_a?(Task) ? parent.id : nil
+
+      # Create access code if present
+      access_code = copy_code(archive_task.access_code, parent.game)
+      # Create task
+      task = Task.create(
+        number: archive_task.number,
+        name: archive_task.name,
+        preview: archive_task.preview,
+        data: archive_task.data,
+        code_quota: archive_task.code_quota,
+        bonus: archive_task.bonus,
+        duration: archive_task.duration,
+        zone_id: zone_id,
+        task_id: task_id,
+        code_id: access_code.try(:id),
+        game_id: parent.game_id
+      )
+      # Copy all Codes
+      archive_task.archive_codes.each {|item| copy_code(item, task) }
+      # Copy all hints
+      archive_task.archive_hints.each {|item| copy_hint(item, task) }
+      # Copy all included task
+      archive_task.archive_tasks.each {|item| copy_task(item, task) }
+
+      task
+    end
+
+    ##
+    # Copy Hint
+    #
+    def copy_hint(archive_hint, parent)
+      return nil unless archive_hint
+      Hint.create(
+          number: archive_hint.number,
+          data: archive_hint.data,
+          delay: archive_hint.delay,
+          cost: archive_hint.cost,
+          task_id: parent.id,
+          game_id: parent.game_id
+      )
+    end
+
+    ##
+    # Copy Code with CodeStrings
+    #
+    def copy_code(archive_code, parent)
+      return nil unless archive_code
+      task_id = parent.is_a?(Task) ? parent.id : nil
+      game_id = parent.is_a?(Game) ? parent.id : parent.game_id
+      code = Code.create(
+          number: archive_code.number,
+          name: archive_code.name,
+          info: archive_code.info,
+          ko: archive_code.ko,
+          color: archive_code.color,
+          bonus: archive_code.bonus,
+          task_id: task_id,
+          game_id: game_id
+      )
+      # Copy all CodeString
+      archive_code.archive_code_strings.each {|item| copy_code_string(item, code) }
+
+      code
+    end
+
+    ##
+    # Copy CodeString from archive
+    #
+    def copy_code_string(archive_code_string, parent)
+      return nil unless archive_code_string
+      CodeString.create(
+          data: archive_code_string.data,
+          color: archive_code_string.color,
+          code_id: parent.id,
+          game_id: parent.game_id
+      )
     end
   end
 
