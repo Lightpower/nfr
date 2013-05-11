73d09b96eab867fa8aad59052a7eb947f58356a7
Vitaliy Beskrovny <bva@aejis.eu>
2013-05-12 02:10:34 +0300

    * flash can contain HTML now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
   #
   def show_flash
     [:notice, :alert, :error].map { |type|
-      content_tag(:div, flash[type], class: type) if flash[type]
+      content_tag(:div, flash[type].html_safe, class: type) if flash[type]
     }.join.html_safe
   end
 
