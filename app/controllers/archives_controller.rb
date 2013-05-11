340e47c433dcdbb50ebaba4d8e67d8007993d745
Vitaliy Beskrovny <bva@aejis.eu>
2013-05-12 02:09:31 +0300

    * bug fix

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -7,16 +7,31 @@ class ArchivesController < ApplicationController
   # List of Games in archive
   #
   def index
-    #@games = Game.where(is_archived: true).order('created_at DESC')
-    @games = Game.order('start_date DESC')
+    @games = Game.where(is_archived: true).order('start_date DESC')
   end
 
   ##
-  #
+  # Show all tasks of archive
   #
   def show
     @game = Game.where(id: params[:id], is_archived: true).first
-
+    @archive_zones = @game.archive_zones.order(:number) if @game.present?
   end
 
+  ##
+  # Show the short statistics of the game
+  #
+  def short_stat
+    @game = Game.where(id: params[:id], is_archived: true).first
+    @stat_result = Stat.total({ game: @game })
+
+    render 'archives/stats/results'
+  end
+
+  ##
+  # Show the statistics of the game
+  #
+  def stat
+
+  end
 end
