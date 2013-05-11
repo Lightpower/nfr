a392013198dee78a1d03c559737dc9af75d61232
Vitaliy Beskrovny <bva@aejis.eu>
2013-05-12 02:10:53 +0300

    + Resultaty

--- app/helpers/archives_helper.rb
+++ app/helpers/archives_helper.rb
@@ -6,9 +6,14 @@ module ArchivesHelper
   #
   def links(game)
     return '' unless game
-    res = link_to('Описание', '#', class: 'archive_link')
-    res << link_to('Статистика', '#', class: 'archive_link')
-    res << link_to('Обсудить', '#', class: 'archive_link')
+    res = link_to('Описание', archive_path(game), class: 'archive_link')
+    res << link_to('Результаты', short_stat_archive_path(game), class: 'archive_link')
+    res << link_to('Статистика', stat_archive_path(game), class: 'archive_link')
+    if game.discuss_url.present?
+      res << link_to('Обсудить', game.discuss_url, class: 'archive_link')
+    else
+      res << link_to('Обсудить', '#', class: 'archive_link')
+    end
     res
   end
 end
