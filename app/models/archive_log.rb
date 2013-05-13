a55edcaf52b736962fa3766fe0c440bd12dc8a0a
Vitaliy Beskrovny <bva@aejis.eu>
2013-05-13 14:50:29 +0300

    + bonus, zone methods

--- app/models/archive_log.rb
+++ app/models/archive_log.rb
@@ -17,4 +17,41 @@ class ArchiveLog < ActiveRecord::Base
     ArchiveTeamCode.where(team_id: team_id, code_id: code_id).try(:first)
   end
 
+  ##
+  # Define bonus of current Log
+  #
+  def bonus
+    # For codes
+    if self.code_id
+      self.team_code.bonus
+    elsif (self.data =~ /^Hint /) == 0
+      # for Hints
+      hint_id = self.data.split(" ")[1].to_i
+      hint = ArchiveHint.where(id: hint_id, game_id: self.game_id).try(:first)
+      if hint
+        hint.cost
+      else
+        0
+      end
+    else
+      0
+    end
+  end
+
+  ##
+  # Define zone of code or hint
+  #
+  def zone
+    # For codes
+    if self.code_id
+      self.team_code.archive_zone
+    elsif (self.data =~ /^Hint /) == 0
+      # for Hints
+      hint_id = self.data.split(" ")[1].to_i
+      hint = ArchiveHint.where(id: hint_id, game_id: self.game_id).try(:first)
+      hint.try(:archive_task).try(:archive_zone)
+    else
+      nil
+    end
+  end
 end
