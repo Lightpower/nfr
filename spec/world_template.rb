1c75a2f0c59c0ca940472705db5b3744778a25e0
Vitaliy Beskrovny <bva@aejis.eu>
2013-07-28 19:03:25 +0300

    * game creating - bugs

--- spec/world_template.rb
+++ spec/world_template.rb
@@ -149,27 +149,36 @@ end
 # 2 teams with user
 #
 def create_line_game
-  game = FactoryGirl.create(:game, game_type: 'line')
-  FactoryGirl.create(:game_config, game: game)
+  debugger
+  project = Project.where(name: 'DozoR').first || Project.create(name: 'DozoR', owner: 'Алесь Жук', css_class: 'dozor')
+  format = Format.where(name: 'Klad').first || Format.create(name: "Klad", organizer: "Lightpower", show_in_archives: true, project: project, css_class: nil)
+  game = Game.find(50) || Game.create(number: "1", name: "Тестовая игра", game_type: "line", start_date: "2013-07-28 15:00:00", finish_date: "2013-07-31 15:00:00", price: 0, area: nil, image_html: "<img src=\"http://neobychnye-zaprosy.ru/pict/box.png\">", preview: nil, legend: 'Тестовая игра и, соответственно, тестовая игра.', brief_place: 'Дома', dopy_list: '- Хорошее настроение', is_active: true, is_archived: false, prepare_url: nil, discuss_url: nil, format: format)
+  GameConfig.create(time: 0, bonus: 0, total_bonus: 0, game: game) if game.config.blank?
   game.reload
 
-  @users = [User.create(email: 'u1@ex.ua', password: '123456', password_confirmation: '123456'),
-            User.create(email: 'u2@ex.ua', password: '123456', password_confirmation: '123456') ]
-  @teams = [Team.create(name: 'Team1', alternative_name: 'Team 1',    image_url: '/images/test.png', user_id: @users.first.id),
-            Team.create(name: 'Team2',  alternative_name: 'Team 2', image_url: '/images/test2.png', user_id: @users.last.id) ]
+  @users = User.where('email ILIKE ?', 'u_@ex.ua') ||
+      [User.create(email: 'u1@ex.ua', password: '123456', password_confirmation: '123456'),
+       User.create(email: 'u2@ex.ua', password: '123456', password_confirmation: '123456') ]
+  @teams = Team.where('name LIKE ?', 'Team_') ||
+      [Team.create(name: 'Team1', alternative_name: 'Team 1',    image_url: '/images/test.png', user_id: @users.first.id),
+       Team.create(name: 'Team2',  alternative_name: 'Team 2', image_url: '/images/test2.png', user_id: @users.last.id) ]
   @users.first.team = @teams.first
   @users.first.save
   @users.last.team = @teams.last
   @users.last.save
   # Requests to game
-  @teams.each { |team| FactoryGirl.create(:game_request, game: game, team: team, is_accepted: true) }
+  GameRequest.where(game_id: game.id, team_id: @teams.map(&:id)).map(&:delete) # delete just in case
+  @teams.each { |team| Team.create(game: game, team: team, is_accepted: true) }
 
-  Zone.create(game: game, number: 1, name: 'Winterfell', image_url: '/image/test1.png')
-  Zone.create(game: game, number: 2, name: 'Casterly Rock', image_url: '/image/test2.png')
+  if game.zones.blank?
+    Zone.create(game: game, number: 1, name: 'Зона 1', image_url: '/image/test1.png')
+    Zone.create(game: game, number: 2, name: 'Зона 2', image_url: '/image/test2.png')
+    game.reload
+  end
 
   # Tasks with codes and included tasks
 
-  Zone.all.each do |zone|
+  game.zones.all.each do |zone|
 
     # task 1 - 1 code
     task_number = zone.new_task_number
@@ -178,7 +187,7 @@ def create_line_game
                        duration: 60, zone: zone)
     # Code
     code_number = task.new_code_number
-    code = Code.create(game: game, number: code_number,
+    code = Code.create(game: game, task: task, number: code_number,
                        name: '',
                        bonus: 0,
                        ko: '1+',
@@ -187,6 +196,7 @@ def create_line_game
     CodeString.create(game: game, data: "D#{zone.number}R#{ + task_number*10 + code_number}", code: code)
     task.codes << code
     task.reload
+    zone.reload
 
     # 2 hints for each task
     Hint.create(game: game, task: task, number: 1, delay: 20, cost: -1, data: '<i>Читайте задание внимательнее!</i>')
@@ -202,7 +212,7 @@ def create_line_game
     infos = %w(Первый Второй Третий Четвёртый Пятый Шестой Седьмой Восьмой Девятый Десятый)
     10.times do
       code_number = task.new_code_number
-      code = Code.create(game: game, number: code_number,
+      code = Code.create(game: game, task: task, number: code_number,
                          name: '',
                          bonus: 0,
                          ko: '1+',
@@ -210,8 +220,9 @@ def create_line_game
                          color: 'blue')
       CodeString.create(game: game, data: "D#{zone.number}R#{ + task_number*10 + code_number}", code: code)
       task.codes << code
+      task.reload
     end
-    task.reload
+    zone.reload
 
     # 2 hints for each task
     Hint.create(game: game, task: task, number: 1, delay: 15, cost: -1, data: '<i>Коды можно подобрать.</i>')
