e775dce57b5fd64e4a80665635d58aed73562fe8
Vitaly Bezkrovny <bva@aejis.eu>
2013-11-26 23:01:33 +0200

    # Line

--- app/models/game_methods/line.rb
+++ app/models/game_methods/line.rb
@@ -1,23 +1,23 @@
 module GameMethods
   class Line
 
-    validate :check_game_type
-
+    #validate :check_game_type
+    #
+    ###
     ##
+    ##
+    #def current_zone(team)
     #
+    #end
     #
-    def current_zone(team)
-
-    end
-
-    def current_task(team)
-
-    end
-
-    private
-
-    def check_game_type
-      errors.add(:game_type, "cannot call method of GameMethods::Line for Game with type '#{self.game_type}'") if self.game_type.classify != 'Line'
-    end
+    #def current_task(team)
+    #
+    #end
+    #
+    #private
+    #
+    #def check_game_type
+    #  errors.add(:game_type, "cannot call method of GameMethods::Line for Game with type '#{self.game_type}'") if self.game_type.classify != 'Line'
+    #end
   end
 end
--- lib/modules/game_strategy/line.rb
+++ lib/modules/game_strategy/line.rb
@@ -4,8 +4,8 @@ module GameStrategy
   # Strategy for Game with game_type = "conquest"
   #
   module Line
-    extend GameStrategy::Line::Actions
-    extend GameStrategy::Line::Rendering
-    extend GameStrategy::Line::ActionPanel
+    #extend GameStrategy::Line::Actions
+    #extend GameStrategy::Line::Rendering
+    #extend GameStrategy::Line::ActionPanel
   end
 end
