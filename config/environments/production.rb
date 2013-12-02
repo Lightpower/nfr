abf3c524b04134499c06e3e1a301d5d153041003
Vitaly Bezkrovny <bva@aejis.eu>
2013-12-02 20:52:35 +0200

    * update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Nfr::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'nfr.aejis.eu' }
+  config.action_mailer.default_url_options = { :host => 'nfr.aejis.biz' }
 end
