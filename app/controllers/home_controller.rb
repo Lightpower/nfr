c3d10ed6f888a4b10c5016144d5b03ab2b8b428f
Vitaly Bezkrovny <bva@aejis.eu>
2013-12-20 00:38:40 +0200

    * return all accessible games

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,18 +2,7 @@
 class HomeController < ApplicationController
 
   def index
-    @games = Game.actual
-    if params[:games]
-      # game format
-      case params[:games][:project]
-        when "other"
-          @games = @games.where(format_id: Format.where(project_id: nil).map(&:id))
-        when nil
-        else
-          project = Project.find_by_name(params[:games][:project])
-          @games = @games.where(format_id: project.formats.map(&:id))
-      end
-    end
+    @games = Game.accessible_by(current_ability).actual
   end
 
 end
