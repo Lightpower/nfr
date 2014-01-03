c412537b25ce30cb0ea2d82eab50ef0b7c4b4f6e
Vitaly Bezkrovny <bva@aejis.eu>
2014-01-03 19:06:07 +0200

    * highlight passed tasks; * show task's number of codes and number of passed codes

--- app/models/task.rb
+++ app/models/task.rb
@@ -55,10 +55,29 @@ class Task < ActiveRecord::Base
   end
 
   ##
+  # If task is finished (necessary codes are passed)
+  # Task can be finished by all necessary codes passing and all included tasks finishing or by deration ending
+  #
+  def is_finished?(team)
+    return false if team.blank?
+
+    # Code quota passed
+    codes_quota = self.code_quota || self.codes.size
+    is_finished_by_codes = passed_code_count(team) >= codes_quota
+    is_finished_by_tasks = !self.tasks || (! self.tasks.map {|tsk| tsk.is_finished?(team)}.include?(false) )
+
+    # Duration is ended
+    is_finished_by_duration = false
+    # TODO: implement finishing the task by duration
+
+    (is_finished_by_codes && is_finished_by_tasks) || is_finished_by_duration
+  end
+
+  ##
   # List of hints which are bought by defined team
   #
   def hints_of(team)
-    TeamHint.includes(:hint).where("team_hints.team_id = ? and hints.task_id = ?", team.id, self.id).order("hints.number").map(&:hint)
+    TeamHint.includes(:hint).where('team_hints.team_id = ? and hints.task_id = ?', team.id, self.id).order('hints.number').map(&:hint)
   end
 
   ##
@@ -66,5 +85,13 @@ class Task < ActiveRecord::Base
   #
   def make_id
     "#{self.class.name.downcase}#{id}"
+  end
+
+  def code_count
+    self.codes.try(:size) || 0
+  end
+
+  def passed_code_count(team)
+    TeamCode.where(team_id: team.id, code_id: self.codes.map(&:id)).size
   end
 end
