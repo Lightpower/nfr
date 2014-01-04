e66c1bbcc0b576968a42144a89cb11325c2d713a
Vitaly Bezkrovny <bva@aejis.eu>
2014-01-04 03:47:59 +0200

    * refactoring ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,90 +1,74 @@
 class Ability
   include CanCan::Ability
 
+  #require 'abilities/admin_abilities'
+  include Abilities::AdminAbilities
+  include Abilities::CaptainAbilities
+  include Abilities::GuestAbilities
+  include Abilities::ModeratorAbilities
+  include Abilities::UserAbilities
+
   def initialize(user)
     user ||= User.new
 
     cannot :all, [:admin_common, :admin_sql]
     cannot [:create, :update, :delete], Game
 
-    if user.role == 'admin'
-      can :manage, :all
-    else
+    # Abilities are ordered by importance
+    abilities_for_guest(user)
+    abilities_for_user(user)
+    abilities_for_captain(user)
+    abilities_for_moderator(user)
+    abilities_for_admin(user)
 
-      # Guest or User without team
-      if user.team.blank?
-        cannot :play, Game
-      end
 
-      # Registered user
-      if user.id
-        can :manage, User,     id: user.id
-        can :read,   User
-        can :create, Team      if user.team.blank?
-        can :read,   Team
-        can :manage, TeamRequest, user_id: user.id, by_user: true
-      end
 
-      # User with team
-      if user.team.present?
-        cannot :create, Team
-      end
-
-      # Captain
-      if user.is_captain?
-        can    :manage, Team,        captain: user
-        cannot :create, Team
-
-        cannot :exclude, User,       id: user.id
-        can :exclude,   User do |u|
-          (u != user) && (u.team_id == user.team_id)
-        end
-
-        cannot :delete, User
-
-        can :manage,    TeamRequest, team_id: user.team_id
-        can :manage,    GameRequest, team_id: user.team_id
-      end
-
-      # Access to Game
-      can :read, Game, is_visible: true
-
-      if user.team.present?
-        cannot :create, Team
-
-        team = user.team
-
-        can [:play, :stat, :log], Game do |game|
-          game.teams.include?(team)
-        end
-      end
+    # Guest or User without team
+    if user.team.blank?
+      cannot :play, Game
     end
 
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user 
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on. 
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/ryanb/cancan/wiki/Defining-Abilities
+    # Registered user
+    if user.id
+      can :manage, User,     id: user.id
+      can :read,   User
+      can :create, Team      if user.team.blank?
+      can :read,   Team
+      can :manage, TeamRequest, user_id: user.id, by_user: true
+    end
+
+    # User with team
+    if user.team.present?
+      cannot :create, Team
+    end
+
+    # Captain
+    if user.is_captain?
+      can    :manage, Team,        captain: user
+      cannot :create, Team
+
+      cannot :exclude, User,       id: user.id
+      can :exclude,   User do |u|
+        (u != user) && (u.team_id == user.team_id)
+      end
+
+      cannot :delete, User
+
+      can :manage,    TeamRequest, team_id: user.team_id
+      can :manage,    GameRequest, team_id: user.team_id
+    end
+
+    # Access to Game
+    can :read, Game, is_visible: true
+
+    if user.team.present?
+      cannot :create, Team
+
+      team = user.team
+
+      can [:play, :stat, :log], Game do |game|
+        game.teams.include?(team)
+      end
+    end
   end
 end
