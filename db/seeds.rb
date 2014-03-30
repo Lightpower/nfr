61d0b708be12ef720d29931cbf0ce559542643c5
Vitaly Bezkrovny <bva@aejis.eu>
2014-03-30 14:43:33 +0300

    + seeds

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# encoding: UTF-8
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require_relative('seeds/seed_new_game')
+
+make_seed
---
+++ db/seeds/helpers.rb
@@ -0,0 +1,19 @@
+##
+# Create code
+#
+def add_code(data, ko, bonus, task, game, info=nil)
+  code_number = task.try(:new_code_number) || 0
+  data = Array.wrap(data)
+  data.map! { |d| Unicode::downcase(d) }
+  code = Code.create(number: code_number, name: '', bonus: bonus, ko: ko, color: 'blue', task_id: task.try(:id), game_id: game.id, info: info)
+  data.each do |d|
+    CodeString.create(data: d, code: code, game_id: game.id)
+  end
+  task.reload if task
+
+  code
+end
+
+def img_tag(filename)
+  "<img src=\"http://klads.org.ua/nfr/006/#{filename}\">"
+end
