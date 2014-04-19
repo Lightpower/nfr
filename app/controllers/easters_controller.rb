7291cd8098fcdeb2ad6b25a86ae0b922fd09a4e2
Vitaly Bezkrovny <bva@aejis.eu>
2014-04-19 12:37:57 +0300

    * easier

--- app/controllers/easters_controller.rb
+++ app/controllers/easters_controller.rb
@@ -7,10 +7,10 @@ class EastersController < ApplicationController
   def miner
     if params[:l]
       field_array = [
-        [73, [4, 16384, 17416, 262240, 67840, 12, 18, 646338, 1344834, 1350850, 1869138, 1350860, 0, 0, 557088, 4608, 98308, 262177, 262144]],
+        [73, [4, 131072, 135176, 160, 532992, 12, 18, 946562, 266882, 291202, 266898, 291212, 0, 0, 262176, 17408, 786436, 33, 0]],
       ]
       i = (Time.now.strftime('%S').to_i / 60)
-      @miner_field_data = [22, 20, field_array[i][0], field_array[i][1].join(',') ].join(';')
+      @miner_field_data = [25, 20, field_array[i][0], field_array[i][1].join(',') ].join(';')
     end
   end
 end
