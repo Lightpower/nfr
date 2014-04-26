b12f4d2ee117e31c2fb60169520c655c1ea72ebb
Vitaly Bezkrovny <bva@aejis.eu>
2014-04-27 02:51:50 +0300

    * message hot fix

--- app/models/game_message.rb
+++ app/models/game_message.rb
@@ -8,7 +8,7 @@ class GameMessage < ActiveRecord::Base
 
   class << self
     def team_game_messages(team_id, game_id)
-      where(game_id: game_id, team_id: team_id).order('created_at DESC')
+      where('game_id=? and(team_id=? or team_id is null)', game_id, team_id).order('created_at DESC')
     end
   end
 end
